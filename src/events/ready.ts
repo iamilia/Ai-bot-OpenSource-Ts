7f251964f3c9843c5fb5ece031a0b5d28bce3f34
ilia <ilialotfi1083@gmail.com>
2022-11-10 13:59:46 +0330

    Update index.ts and events/ready.ts
    add Error Hnadle
    add user activity set

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,30 @@
+// ActivityType.Competing = 5
+// ActivityType.Listening = 2
+// ActivityType.Playing = 0 
+// ActivityType.Watching = 3
+let lastst: string;
+function Setst(client: any) :void {
+    const st = ["dnd", "idle", "online"]
+    const sta = st[Math.floor(Math.random() * st.length)]
+    if (lastst == sta) return;
+    lastst = sta
+    client.user.setStatus(`${sta}`)
+}
+function SetAt(client: any) :void {
+    const statusArray = ['Playing WIth Ts, 0', `Watching GithubSorce, 3`];
+    const random = statusArray[Math.floor(Math.random() * statusArray.length)].split(', ')
+    const status = random[0];
+    const mode = parseInt(random[1]);
+    client.user?.setActivity({ name: status, type: mode })
+}
 export default {
-  name: "ready",
-
-  run: async (client: any) => {
-    console.log(`${client.user.tag} is up and ready to go!`);
-    console.log(`${client.guilds.cache.size} servers, ${client.users.cache.size} members`);
-  },
+    name: "ready",
+    run: async (client: any) => {
+        console.log(`${client.user.tag} is up and ready to go!`);
+        console.log(`${client.guilds.cache.size} servers, ${client.users.cache.size} members`);
+        setInterval(() => {
+            Setst(client)
+            SetAt(client)
+        }, 35000);
+    },
 }
--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import handler from "./handler";
 import { Client, Collection } from "discord.js";
 
 declare module "discord.js" {
-  export interface Client {
-    commands: Collection<string, any>;
-    buttons: Collection<string, any>;
-    modals: Collection<string, any>;
-  }
+    export interface Client {
+        commands: Collection<string, any>;
+        buttons: Collection<string, any>;
+        modals: Collection<string, any>;
+    }
 }
 
 const client = new Client({ intents: 32767 });
@@ -19,8 +19,32 @@ client.commands = new Collection();
 client.buttons = new Collection();
 
 (async () => {
-  await handler(client);
-
-  await client.login(process.env.TOKEN);
-  console.log(`${client.user?.tag} is Online `);
+    await handler(client);
+    await client.login(process.env.TOKEN);
 })();
+
+// ———————————————[Error Handling]———————————————
+process.on("unhandledRejection", (reason, p) => {
+    console.log("—————————————————————————————————");
+    console.log(
+        "[",
+        "AntiCrash",
+        "]",
+        " : ",
+        "Uncaught Exception/Catch"
+    );
+    console.log(reason, p);
+    console.log("—————————————————————————————————");
+});
+process.on("uncaughtException", (err, origin) => {
+    console.log("—————————————————————————————————");
+    console.log(
+        "[",
+        "AntiCrash",
+        "]",
+        " : ",
+        "Uncaught Exception/Catch"
+    );
+    console.log(err, origin);
+    console.log("—————————————————————————————————");
+});
