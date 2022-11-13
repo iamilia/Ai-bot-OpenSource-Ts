09057a7a3aa1247e67ec5841842e731b12728b17
ilia <ilialotfi1083@gmail.com>
2022-11-13 13:37:09 +0330

    Update interactionCreate.ts

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,25 +1,24 @@
-import { ChatInputCommandInteraction } from "discord.js";
+import { ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
 import client from "../index";
 
 export default {
-  name: "interactionCreate",
-  run: async (interaction: ChatInputCommandInteraction) => {
-    if (!interaction.isCommand()) return;
-    const command = client.commands.get(interaction.commandName);
-    if (!command) {
-      return interaction.followUp({
-        content: "An error occured while running the command",
-      });
-    } else {
-      try {
-        await command.run(client, interaction);
-      } catch (error) {
-        await interaction.reply({
-          content: "There was an error while executing this command!",
-          ephemeral: true,
-        });
-        console.error(error);
-      }
-    }
-  },
+    name: "interactionCreate",
+    run: async (interaction: ChatInputCommandInteraction) => {
+        if (!interaction.isCommand()) return;
+        const command = client.commands.get(interaction.commandName);
+        if (!command) {
+            return interaction.followUp({
+                content: "An error occured while running the command",
+            });
+        } else {
+            try {
+                await command.run(client, interaction);
+            } catch (error) {
+                console.log(error);
+                await interaction[interaction.deferred ? 'editReply' : interaction.replied ? 'followUp' : 'reply']({
+                    embeds: [new EmbedBuilder().setColor('Red').setDescription(`${error}` || 'Unexpected error')]
+                });
+            }
+        }
+    },
 };
